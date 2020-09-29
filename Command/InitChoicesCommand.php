18b2a22736cf5109fa95a0501e9a96818c4f99f8
François Pluchino <francois.pluchino@klipper.dev>
2020-09-29 08:58:04 +0200

    Find all choices data files in project and bundles

--- Command/InitChoicesCommand.php
+++ Command/InitChoicesCommand.php
@@ -18,6 +18,7 @@ use Klipper\Component\Resource\Domain\DomainManagerInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Finder\Finder;
 
 /**
  * Init the system choices.
@@ -30,12 +31,15 @@ class InitChoicesCommand extends Command
 
     private string $projectDir;
 
-    public function __construct(DomainManagerInterface $domainManager, string $projectDir)
+    private array $bundles;
+
+    public function __construct(DomainManagerInterface $domainManager, string $projectDir, array $bundles)
     {
         parent::__construct();
 
         $this->domainManager = $domainManager;
         $this->projectDir = $projectDir;
+        $this->bundles = $bundles;
     }
 
     protected function configure(): void
@@ -51,28 +55,66 @@ class InitChoicesCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
-        $domainChoice = $this->domainManager->get(ChoiceInterface::class);
-        $loader = new YamlChoiceLoader($domainChoice);
-        $file = $this->projectDir.'/config/data/choices.yaml';
+        $finder = (new Finder())
+            ->ignoreVCS(true)
+            ->in($this->getBundlePaths())
+            ->name([
+                'choices.yaml',
+                'choices_*.yaml',
+            ])
+        ;
 
-        if (!file_exists($file)) {
-            $output->writeln(sprintf('  The system choices are not defined in file "%s"', $file));
+        if (0 === $finder->count()) {
+            $output->writeln('  No system choices are defined in "<project_dir>/config/data" or "<bundle>/Resources/config/data"');
 
             return 0;
         }
 
-        if (!$loader->supports($file)) {
-            throw new InvalidArgumentException('The resource is not supported by this data loader');
+        $domainChoice = $this->domainManager->get(ChoiceInterface::class);
+        $loader = new YamlChoiceLoader($domainChoice);
+        $updated = false;
+
+        foreach ($finder->files() as $file) {
+            $filename = $file->getPathname();
+
+            if (!$loader->supports($filename)) {
+                throw new InvalidArgumentException(sprintf(
+                    'The resource "%s" is not supported by this data loader',
+                    $filename
+                ));
+            }
+
+            $loader->load($filename);
+            $updated = $updated || $loader->hasNewEntities() || $loader->hasUpdatedEntities();
         }
 
-        $loader->load($file);
-
-        if ($loader->hasNewEntities() || $loader->hasUpdatedEntities()) {
+        if ($updated) {
             $output->writeln('  The system choices have been initialized');
         } else {
             $output->writeln('  The system choices are already up to date');
         }
 
         return 0;
+    }
+
+    /**
+     * @return string[]
+     */
+    protected function getBundlePaths(): array
+    {
+        $paths = [];
+
+        foreach ($this->bundles as $bundle) {
+            $ref = new \ReflectionClass($bundle);
+            $path = \dirname($ref->getFileName()).'/Resources/config/data';
+
+            if (is_dir($path)) {
+                $paths[] = $path;
+            }
+        }
+
+        $paths[] = $this->projectDir.'/config/data';
+
+        return $paths;
     }
 }
